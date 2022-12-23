39f74695140f7abef03e4134d0fc8e72282d847f
Pawan Gonnakuti <pawangonnakuti@users.noreply.github.com>
2022-12-24 11:19:41 +1300

    updated libcamera-vid command to support HD video streaming

--- homecam-client.sh
+++ homecam-client.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-libcamera-vid --timeout 0000 --nopreview --inline --listen -o tcp://0.0.0.0:55000
+libcamera-vid --nopreview --bitrate 10000000 --codec h264 --framerate 30 --width 1920 --height 1080 --timeout 0000 --nopreview --inline --listen -o tcp://0.0.0.0:8888
 
 echo "Command exited with code $?"
 
